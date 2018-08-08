3429e3ea61d0c0623fef9a5c0413721b3213afd0
nicholasting92 <nicholasting92@gmail.com>
2018-08-08 11:34:37 +1000

    Number fun

--- Kattis/number_fun.py
+++ Kattis/number_fun.py
@@ -1,1 +1,25 @@
+
+
 num_cases = int(input())
+
+while num_cases != 0:
+    a,b,c = map(int, input().split())
+
+    if a + b == c:
+        print("Possible")
+    
+    elif a - b == c or b - a == c:
+        print("Possible")
+    
+    elif a * b == c:
+        print("Possible")
+    
+    elif a / b == c or b / a == c:
+        print("Possible")
+    
+    else:
+        print("Impossible")
+
+    num_cases -= 1
+    
+    
